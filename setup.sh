462d3d700ddbaae93a10636b7c36ffc3d182b38d
Elichai Turkel <elichai.turkel@gmail.com>
2018-03-27 21:54:38 +0300

    Add a setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Trying to uninstall existing module.."
+sudo systemctl stop aesmd
+sudo /sbin/modprobe -r isgx
+sudo /sbin/modprobe -r graphene_sgx
+sudo /sbin/modprobe -r isgx
+sudo rm -rf "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"
+sudo /sbin/depmod
+sudo /bin/sed -i '/^isgx$/d' /etc/modules
+
+make
+
+set -e
+
+echo "Installing kernel module..."
+sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
+sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
+sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules"    
+sudo /sbin/depmod
+sudo /sbin/modprobe isgx
